6035f158c1675b7637997b49f83369db2a051aa2
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2025-03-12 15:37:26 +0530

    Uploaded Assignments on 12-03 9:07 PM

---
+++ Week 2 Checkpoint/Task 5 - Department Business.py
@@ -0,0 +1,37 @@
+print("CHECK FILENAMES")
+valid_departments = ['dep1', 'dep2', 'dep3']
+# Use a while loop to get user input until the user enters "quit"
+while True:
+    # Take input for department name
+    department_name = input("Input a department (or quit to exit): ")
+
+    # Using while loop to repeatedly take input for department name (if "" was entered earlier)
+    while not department_name:
+        department_name = input("Input a department (or quit to exit): ")
+
+    if department_name == 'quit':
+        break
+
+    # Checking if department name is valid
+    if department_name not in valid_departments:
+        continue
+
+    else:
+        # Take input for file name
+        file_name = input("Input a filename: ")
+
+        # Using while loop to get file name
+        while not file_name:
+            file_name = input("Input a filename: ")
+
+        # Checking if the file name is valid for the corresponding department name
+        if ((department_name == 'dep1' and file_name in ['filea', 'fileb'])
+                or (department_name == 'dep2' and file_name in ['filec'])
+                or (department_name == 'dep3' and file_name in ['filea', 'filec'])):
+
+            print(f"{file_name} - is a valid filename for - {department_name}")
+
+        else:
+            print(f"{file_name} - *is not* a valid filename for - {department_name}")
+
+print("GOODBYE")
---
+++ Week 4 Checkpoint/Task 7 - Full Cypher.py
@@ -0,0 +1,42 @@
+print("DECRYPT STRING")
+encrypted_string = input("Input encrypted string: ")
+
+# Initialize an empty string to store the decrypted string
+decrypted_string = ""
+
+# Checking in input encrypted string is empty
+if encrypted_string.strip() == "":
+    print("Empty encrypted string.")
+    exit(0)
+
+else:
+    caesar_shift = int(input("Input caesar shift: "))
+
+    # Checking for invalid caesar shift and exit the program, if true
+    if caesar_shift < 0 or caesar_shift > 25:
+        print("Invalid caesar shift.")
+        exit(0)
+
+    else:
+        for character in encrypted_string:
+
+            # Skip spaces. No decoding
+            if character == " ":
+                continue
+
+            '''ord(character) gives ASCII value and ord(character) - 65 gives the integer values in alphabet table
+            for each alphabet. The next part is just subtracting caesar shift and getting the new value of shift
+            which will be used for the next character in encrypted string.'''
+            caesar_shift = (ord(character) - 65 - caesar_shift) % 26
+
+            """char(integer) results in a character whose ASCII value is that integer and we add 65 to get alphabets 
+            in range A-Z. The resulting character is appended to the decrypted string."""
+            decrypted_string += chr(caesar_shift + 65)
+
+    # Printing out the characters in the decrypted string
+    print("The decrypted string is:")
+    for character in decrypted_string:
+        print(character)
+
+
+
